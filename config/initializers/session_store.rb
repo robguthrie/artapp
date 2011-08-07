f7d35d900eb2b22712c04eb9b4e6071ae0356ad7
Robert Guthrie <rguthrie@gmail.com>
2011-08-07 18:01:05 +1200

    okokok

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Artapp::Application.config.session_store :cookie_store, key: '_artapp_session'
+Artapp::Application.config.session_store :cookie_store, :key => '_artapp_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
