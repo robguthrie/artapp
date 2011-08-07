1c5c5f398feb8c75f42f8ec391ac7f2f6f796f0b
Robert Guthrie <rguthrie@gmail.com>
2011-08-07 19:09:16 +1200

    okok

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,5 @@
 class ArtistsController < ApplicationController
+  def create
+    create!{new_artist_artwork_path(@artist)}
+  end
 end
--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,3 +1,6 @@
 class ArtworksController < ApplicationController
   belongs_to :artist, :optional => true 
+  def create
+    create!{@artist}
+  end
 end
