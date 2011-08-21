64c9463056d94fa3984cf12c9261d894a22802ff
Robert Guthrie <rguthrie@gmail.com>
2011-08-21 15:18:10 +1200

    bids work..just

--- app/controllers/admin/artworks_controller.rb
+++ app/controllers/admin/artworks_controller.rb
@@ -3,7 +3,7 @@ class Admin::ArtworksController < AdminController
     @total_raised = 0
     collection.each do |a|
       if a.auction_physical and a.leading_bid
-        if a.leading_bid.amount >= a.auction_reserve_price
+        if a.auction_reserve_price.nil? or a.leading_bid.amount >= a.auction_reserve_price
           @total_raised += a.leading_bid.amount
         end
       end
