9a5d1e94a0a67bf336d515f7ba83f8d9433d8fb3
Robert Guthrie <rguthrie@gmail.com>
2011-08-30 14:41:04 +1200

    late check

--- app/controllers/admin/bids_controller.rb
+++ app/controllers/admin/bids_controller.rb
@@ -1,5 +1,5 @@
 class Admin::BidsController < AdminController
-
+  belongs_to :artwork, :optional => true
   def index
     if params[:late]
       @bids = Bid.where('created_at > ?', Chronic.parse('2011-08-26 8pm'))
